5b892cc201dd928cc1ba9f0c9ebc9b492abe188d
Laura-Foxey <roura_sasunaru@hotmail.com>
2022-10-13 19:01:32 +0300

    change portofolio page

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -5,6 +5,7 @@ import tarot3 from '../images/tarot-reader-scrn3.png';
 import twerp1 from '../images/twerp-scrn1.png';
 import twerp2 from '../images/twerp-scrn2.png';
 import twerp3 from '../images/twerp-scrn3.png';
+import ghostbuster1 from  '../images/ghost-buster-scrn1.png';
 import SimpleImageSlider from "react-simple-image-slider";
 import { motion} from "framer-motion"
 
@@ -21,16 +22,49 @@ function Portofolio({direction}) {
         {url: twerp3},
     ];
 
-    return (
-        <motion.div className="portofolio template"
-        initial= {{x: direction > 0 ? -100: 100}}
-        animate={{x: 0, opacity: 1}}
-        >
-            <div className='slider-box'>
-                <section> a</section>
-                <section> b</section>
+    const Twerp =
+    <a href='https://github.com/LucasCNordstrom/Loop5FinalProject' target="_blank" rel='noreferrer'>
+        <div className='project-card'>
+            <h3>Twerp</h3>
+            <h5>Final group project for Salt's curriculum</h5>
+            <h5>Webapp for storing and tracking perishables's expiration date.</h5>
+            <img src={twerp1} alt="twerp-prntscreen" width={250}/>
+        </div>
+    </a>
+
+    const Tarot =
+    <a href='https://github.com/Laura-Foxey/tarotReader' target="_blank" rel='noreferrer'>
+        <div className='project-card'>
+            <h3>Tarot Reader</h3>
+            <h5>Hackday project built in 24h</h5>
+            <h5>Webapp in the style of a game to simulate having your cards read.</h5>
+            <img src={tarot1} alt="twerp-prntscreen" width={250}/>
+        </div>
+    </a>
+
+    const Ghostbuster =
+        <a href='https://github.com/Laura-Foxey/Ghost-Buster' target="_blank" rel='noreferrer'>
+            <div className='project-card'>
+                <h3>Ghost Buster</h3>
+                <h5>Windows Forms game in C#</h5>
+                <h5>Shoot ghosts with this simple game and try not to die!</h5>
+                <img src={ghostbuster1} alt="twerp-prntscreen" width={250}/>
             </div>
-        </motion.div>
+        </a>
+
+    return (
+        <>
+            <motion.div className="portofolio template"
+            initial= {{x: direction > 0 ? -100: 100}}
+            animate={{x: 0, opacity: 1}}
+            >
+                <div className='slider-box'>
+                    <section> {Twerp} </section>
+                    <section> {Tarot} </section>
+                    <section> {Ghostbuster} </section>
+                </div>
+            </motion.div>
+        </>
     );
   }
   
